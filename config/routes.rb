9b1488c9966c18481f9f1426d6180837903f6af7
Haruto0309 <sato.haruto.infratop@gmail.com>
2024-09-24 07:45:20 +0000

    [FINISH] 4-13

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ devise_for :users
 root to: "homes#top"
 get "homes/about", as:"about"
 
-resources :post_images, only:[:new, :index, :show, :create]
+resources :post_images, only:[:new, :index, :show, :create, :destroy]
 end
